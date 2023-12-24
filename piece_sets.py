04460c7d9b4a46aea34d72f748875b7eaae4cb69
lsomoza6464 <149111617+lsomoza6464@users.noreply.github.com>
2023-12-24 13:11:50 -0800

    Update piece_sets.py

--- piece_sets.py
+++ piece_sets.py
@@ -7,7 +7,8 @@ white_set = {(0,0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0), (0,1
 black_set = {(0,7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7), (0,6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)}
 captured_pieces_white = set()
 captured_pieces_black = set()
-en_passant_pawns = set()
+white_en_passant_pawns = set()
+black_en_passant_pawns = set()
 black_king_location = (3, 7)
 white_king_location = (3, 0)
 """
